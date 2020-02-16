70f725b1e228430ae23c63e93075132355227203
niranjan131 <57741875+niranjan131@users.noreply.github.com>
2020-02-16 16:54:05 +0530

    problem rectified

--- bub.py
+++ bub.py
@@ -1,6 +1,6 @@
 l=[2,42,3,67,5,6,88]
-for i in range(0,len(l)-1):
-	 for j in range(i+1,len(l)-1):
-		if(l[i]>l[i+1]):
-			l[i],l[i+1]=l[i+1],l[i]	
-print("sorted:",l)	
+for i in range(0, len(l)-1):
+	 for j in range(i+1, len(l)-1):
+		 if l[i]>l[j]:
+			 l[i],l[j]=l[j],l[i]
+print("sorted:",l)
